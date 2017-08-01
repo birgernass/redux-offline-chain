8dce4fdb9afcdfcbead56f3fb3cb7f9d37b002a5
Birger Naß <n.b@posteo.de>
2017-08-01 23:35:21 +0200

    changed dispatch order

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 export default ({ dispatch, getState }) => next => action => {
+  const result = next(action)
   if (action.meta && typeof action.meta.then === 'function') {
     const invoked = action.meta.then(action.payload)
     typeof invoked === 'function'
       ? invoked(dispatch, getState)
       : dispatch(invoked)
   }
-  return next(action)
+  return result
 }
