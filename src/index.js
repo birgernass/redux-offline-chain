5606126a2793f499b62127ce003380a9c011ce12
Birger Naß <n.b@posteo.de>
2017-11-13 13:52:37 +0100

    use if statement for better readability

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ export default ({ dispatch, getState }) => next => action => {
   const result = next(action)
   if (action.meta && typeof action.meta.then === 'function') {
     const invoked = action.meta.then(action.payload)
-    typeof invoked === 'function'
-      ? invoked(dispatch, getState)
-      : dispatch(invoked)
+    if (typeof invoked === 'function') {
+      invoked(dispatch, getState)
+    } else {
+      dispatch(invoked)
+    }
   }
   return result
 }
